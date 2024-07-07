087d226df51c44e65347183ca431b038f0458c6c
aimanny24 <ai.manny@outlook.com>
2024-07-07 10:27:29 -0400

    "Updated contents of folders Fundamentals and Mocking"

---
+++ SampleProject/Fundamentals/Reservation.cs
@@ -0,0 +1,20 @@
+﻿namespace TestNinja.Fundamentals
+{
+    public class Reservation
+    {
+        // a property which specifies the user that makes the reservation
+        public User MadeBy { get; set; }
+
+        public bool CanBeCancelledBy(User user)
+        {
+            return (user.IsAdmin || MadeBy == user);
+        }
+        
+    }
+
+    // a User class with one property
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+}
